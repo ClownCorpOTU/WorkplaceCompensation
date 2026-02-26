064e17c2c1f8bae715ab7c6e13eb3942ef8bf63d
IzzyDev <kishoreishaan@gmail.com>
2026-02-26 18:12:25 -0500

    Added variations to the audio manager

--- Assets/_Scripts/Audio/Sound.cs
+++ Assets/_Scripts/Audio/Sound.cs
@@ -6,6 +6,8 @@ public class Sound
 {
     public string name;
     public AudioClip clip;
+    public bool hasVariations;
+    public Sound[] variations;
     public AudioMixerGroup audioMixerGroup;
 
     [Range(0f, 1f)] public float volume;
